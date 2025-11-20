377a2aa3c21a0a1b4b0cfe0b8dbb08870e32a309
Kenny Daniel <platypii@gmail.com>
2025-11-19 16:57:35 -0800

    Initial JS project

---
+++ test/package.test.js
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest'
+import packageJson from '../package.json' with { type: 'json' }
+
+describe('package.json', () => {
+  it('should have the correct name', () => {
+    expect(packageJson.name).toBe('parquet-grep')
+  })
+  it('should have a valid version', () => {
+    expect(packageJson.version).toMatch(/^\d+\.\d+\.\d+$/)
+  })
+  it('should have MIT license', () => {
+    expect(packageJson.license).toBe('MIT')
+  })
+  it('should have precise dependency versions', () => {
+    const { devDependencies } = packageJson
+    Object.values(devDependencies).forEach(version => {
+      expect(version).toMatch(/^\d+\.\d+\.\d+$/)
+    })
+  })
+  it('should have no dependencies', () => {
+    expect('dependencies' in packageJson).toBe(false)
+  })
+})
